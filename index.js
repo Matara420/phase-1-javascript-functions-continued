63c540d4bf6442c00d2a7e5671390f71a74b8794
Jesse Matara <jesse.matara@student.moringaschool.com>
2025-06-10 17:07:28 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function appendOutput(message) {
+    console.log(message);
+}
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+appendOutput(saturdayFun());
+appendOutput(saturdayFun('hike'));
+
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+appendOutput(mondayWork());
+appendOutput(mondayWork('learn JavaScript'));
+
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+const encouragingPromptFunction = wrapAdjective("!!!");
+appendOutput(encouragingPromptFunction("amazing"));
+appendOutput(wrapAdjective("%")("a dedicated programmer"));
